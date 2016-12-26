b1f3c757864d2807644be33bea8dbca6e0f6d58a
DAVIDE RAMONDETTI <info@ramondettidavide.com>
2016-12-26 22:29:47 +0100

    Create config.php

---
+++ MEGAPwLock/config.php
@@ -0,0 +1,10 @@
+<?php
+/* © 2017 RamondettiDavide.com */
+
+//-- EDIT HERE --//
+$authWebsite = "http://yourwebsite.com"; # type here your website with the protocol and without the slash (eg. http://yourwebsite.com)
+
+//-- DO NOT EDIT BEFORE THIS LINE--//
+function GenSecUrl() {
+  return "https://ramondetti.be/SecURL.php";
+}
